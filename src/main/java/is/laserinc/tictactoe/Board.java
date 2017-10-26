c1243954f2ddef04dd94f6533d720192ea7a4d1d
unknown <thorirv15@ru.is>
2017-10-26 09:30:10 +0000

    Added feature: initializeBoard

--- src/main/java/is/laserinc/tictactoe/Board.java
+++ src/main/java/is/laserinc/tictactoe/Board.java
@@ -2,8 +2,28 @@ package is.laserinc.tictactoe;
 import java.util.*;
 
 public class Board {
+	private char board[][];
+	private int boardSize = 3;
 
-	public static void main(String[] args) {
-		
+	// Initialize new board of size 3x3.
+	public Board() {
+		board = new char[3][3];
+		initializeBoard();
 	}
+
+	public void initializeBoard() {
+		int counter = 1;
+		for(int i = 0; i < boardSize; i++) {
+			for(int j = 0; j < boardSize; j++) {
+				char number = Integer.toString(counter).charAt(0);
+				board[i][j] = number;
+				counter++;
+			}
+		}
+	}
+
+	public char[][] getBoard() {
+			return this.board;
+	}
+
 }
