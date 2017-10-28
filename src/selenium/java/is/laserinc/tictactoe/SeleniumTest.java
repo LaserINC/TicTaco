cfa6df37c6eb3b9fd095e92a78a951e359409d65
unknown <thorirv15@ru.is>
2017-10-28 14:50:51 +0000

    Seleninum tests and seleniumWrapper complete

--- src/selenium/java/is/laserinc/tictactoe/SeleniumTest.java
+++ src/selenium/java/is/laserinc/tictactoe/SeleniumTest.java
@@ -1,34 +1,77 @@
 package is.laserinc.tictactoe;
 
-import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.openqa.selenium.By;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-
-
-public abstract class SeleniumTest {
-  static ChromeDriver driver;
-  static String baseUrl;
-  static String port;
-
-  @BeforeClass
-  public static void openBrowser() {
-    driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-    port = System.getenv("PORT");
-    if (port == null) {
-        port = "4567";
-    }
-    baseUrl = "http://localhost:" + port;
-    //baseUrl = "https://tictactoe-assignment.herokuapp.com/"; // + port;
-  }
- 
-  //@AfterClass
-  //public static void closeBrowser(){
-   //driver.quit();
-  //}
-}
+public class SeleniumTest extends SeleniumWrapper {
   
+  private String gameString;
+
+  @Test
+  public void testTitleMatches() {
+    driver.get(baseUrl);
+    assertEquals("TIC TAC TOE", driver.getTitle());
+  }
+
+  @Test
+  public void testGameXWin() throws Exception {
+
+    gameString = "14253";
+
+    for(int i = 0; i < gameString.length(); i++) {
+      driver.findElement(By.id(Character.toString(gameString.charAt(i)))).click();
+      Thread.sleep(1000);
+    }
+    
+    String results = driver.findElement(By.id("message_prompt")).getText();
+    assertEquals("Winner is x!", results);
+
+  }
+
+  @Test
+  public void testGameOWin() throws Exception {
+    
+    gameString = "514293";
+
+    for(int i = 0; i < gameString.length(); i++) {
+      driver.findElement(By.id(Character.toString(gameString.charAt(i)))).click();
+      Thread.sleep(1000);
+    }
+
+    String results = driver.findElement(By.id("message_prompt")).getText();
+    assertEquals("Winner is o!", results);
+
+  }
+
+
+  @Test
+  public void testGameTie() throws Exception{
+
+    gameString = "123546879";
+
+    for(int i = 0; i < gameString.length(); i++) {
+      driver.findElement(By.id(Character.toString(gameString.charAt(i)))).click();
+      Thread.sleep(1000);
+    }
+
+    String results = driver.findElement(By.id("message_prompt")).getText();
+    assertEquals("Tie game", results);
+
+  }
+
+  @Test
+  public void testNumberHasAlreadyBeenUsed() throws Exception {
+
+    gameString = "1421";
+
+    for(int i = 0; i < gameString.length(); i++) {
+      driver.findElement(By.id(Character.toString(gameString.charAt(i)))).click();
+      Thread.sleep(1000);
+    }
+
+    String results = driver.findElement(By.id("message_prompt")).getText();
+    assertEquals("Number has already been used", results);
+
+  }
+}
