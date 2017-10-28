123ff782eae37b73621a5013479a73fcaccb9c40
unknown <thorirv15@ru.is>
2017-10-28 12:39:53 +0000

    Test lina i webGame farin

--- src/main/java/is/laserinc/tictactoe/WebGame.java
+++ src/main/java/is/laserinc/tictactoe/WebGame.java
@@ -31,7 +31,6 @@ public class WebGame {
 	public static void main(String[] args) {
 		staticFileLocation("/public");
 		port(readPortOrDefault());
-		//get("/", (req, res) -> "<button> takki </button>" );
 		WebGame g = new WebGame();
 
 		post("/makeMove",
